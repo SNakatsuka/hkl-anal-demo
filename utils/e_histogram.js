0a952aaf85f70183645e0f389d7f3e18d0360645
31P <34903407+SNakatsuka@users.noreply.github.com>
2026-01-23 11:17:05 +0100

    Fix typo in setting rectangle fill attribute

--- utils/e_histogram.js
+++ utils/e_histogram.js
@@ -59,7 +59,7 @@ export function renderEHistogramSVG(containerEl, hist, opts={}) {
     const rect = document.createElementNS(svgns, "rect");
     rect.setAttribute("x", x);
     rect.setAttribute("y", y);
-   Attribute("fill", "#38bdf8");
+    rect.Attribute("fill", "#38bdf8");
     svg.appendChild(rect);
   }
 
