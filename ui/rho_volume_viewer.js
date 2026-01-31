b819c81c42309bff5c0bd5f455316111651f4518
31P <34903407+SNakatsuka@users.noreply.github.com>
2026-01-31 20:38:43 +0100

    Create rho_volume_viewer.js

---
+++ ui/rho_volume_viewer.js
@@ -0,0 +1,6 @@
+// ui/rho_volume_viewer.js
+import { renderPattersonVolumeViewer } from './patterson_volume_viewer.js';
+
+export function renderRhoVolumeViewer(container, rho) {
+  renderPattersonVolumeViewer(container, rho);
+}
